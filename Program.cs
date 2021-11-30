9d4e11888fef35ef2d2852102a40096aa8bcc4b3
initiae31 <initiae31@gmail.com>
2021-12-01 01:02:35 +1400

    Implement BMI calculator

--- Program.cs
+++ Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press 1 to calculate BMI. Press 2 to view membership rates:");
+            char selection = Console.ReadKey().KeyChar;
+            if (selection == '1')
+            {
+                Console.WriteLine("\nPlease enter your weight in kilograms:");
+                var weightInput = Console.ReadLine();
+                double weight = 0;
+
+                // Handle error re-prompt user for input
+                while (!double.TryParse(weightInput, out weight))
+                {
+                    Console.WriteLine("Incorrect input please re-enter weight:");
+                    weightInput = Console.ReadLine();
+                    weight = 0;
+                }
+
+                Console.WriteLine("Please enter your height in meters:");
+                var heightInput = Console.ReadLine();
+                double height = 0;
+
+                // Handle error re-prompt user for input
+                while (!double.TryParse(heightInput, out height))
+                {
+                    Console.WriteLine("Incorrect input please re-enter height:");
+                    heightInput = Console.ReadLine();
+                    height = 0;
+                }
+
+                double BMI = weight / (height * height);
+                Console.WriteLine($"Your BMI is: {BMI:0.##}");
+
+                if (BMI < 18.5)
+                {
+                    Console.WriteLine("You are underweight!");
+                }
+
+                else if (BMI <= 25)
+                {
+                    Console.WriteLine("You are normal weight!");
+                }
+
+                else if (BMI <= 30)
+                {
+                    Console.WriteLine("You are overweight!");
+                }
+
+                else
+                {
+                    Console.WriteLine("You are obese!");
+                }
+
+
+            }
+            else if (selection == '2')
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
